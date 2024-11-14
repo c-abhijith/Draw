a28ae53261a977431709be0472cbf202222d6720
c-abhijith <cabhijithofficial@gmail.com>
2024-11-14 13:21:58 +0530

    new

--- config.py
+++ config.py
@@ -9,8 +9,9 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')  # Default key for development
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')  
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'static/uploads/')  # Default upload folder
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Cloudinary configuration
     CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
     CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
     CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
-    UPLOAD_FOLDER = "static/uploads"
